fb21750e5c5ffa4ad49565eaa4630b121b42443d
Dad <107422850+dad84@users.noreply.github.com>
2023-02-26 14:42:14 +0100

    Create TestSnake.py

---
+++ v2/TestSnake.py
@@ -0,0 +1,146 @@
+import random
+
+# Define the state space of the Snake game
+state_space = 4
+
+# Define the size of the population
+population_size = 50
+
+# Define the number of generations to run the algorithm
+num_generations = 100
+
+def fitness_function(genotype):
+    # Define the size of the game board
+    board_width = 20
+    board_height = 20
+    
+    # Define the initial position and velocity of the Snake
+    snake_x = board_width // 2
+    snake_y = board_height // 2
+    snake_dx, snake_dy = 0, -1
+    
+    # Define the initial position of the food
+    food_x, food_y = random.randint(0, board_width-1), random.randint(0, board_height-1)
+    
+    # Define the initial state of the game
+    score = 0
+    snake_length = 1
+    snake_list = [(snake_x, snake_y)]
+    game_over = False
+    
+    # Run the game for a fixed number of steps
+    max_steps = 100
+    for step in range(max_steps):
+        # Update the position of the Snake based on its velocity
+        snake_x += snake_dx
+        snake_y += snake_dy
+        
+        # Check if the Snake has collided with the walls or its own body
+        if snake_x < 0 or snake_x >= board_width or snake_y < 0 or snake_y >= board_height:
+            game_over = True
+        for segment in snake_list[:-1]:
+            if segment == (snake_x, snake_y):
+                game_over = True
+        
+        # Check if the Snake has eaten the food
+        if snake_x == food_x and snake_y == food_y:
+            score += 1
+            snake_length += 1
+            food_x, food_y = random.randint(0, board_width-1), random.randint(0, board_height-1)
+        
+        # Update the position of the Snake's body
+        snake_list.append((snake_x, snake_y))
+        if len(snake_list) > snake_length:
+            del snake_list[0]
+        
+        # Update the velocity of the Snake based on its genotype
+        input_vector = [snake_x-food_x, snake_y-food_y, snake_dx, snake_dy]
+        output_vector = [sum([input_vector[i]*genotype[i] for i in range(len(genotype))])]
+        snake_dx += int(output_vector[0])
+        snake_dy += int(output_vector[0])
+        
+        # Check if the game is over
+        if game_over:
+            break
+    
+    # Return the fitness score based on the final score of the game
+    return score
+
+
+def selection_function(population):
+    # Define the tournament size
+    tournament_size = 5
+    
+    # Randomly select a set of individuals from the population
+    tournament_indices = random.sample(range(len(population)), tournament_size)
+    tournament_scores = [fitness_function(population[i]) for i in tournament_indices]
+    
+    # Return the index of the fittest individual in the tournament
+    return tournament_indices[tournament_scores.index(max(tournament_scores))]
+
+
+def crossover_function(parent1, parent2):
+    # Define the probability of crossover
+    crossover_prob = 0.5
+    
+    # Perform single-point crossover between the parents with a probability of crossover_prob
+    if random.random() < crossover_prob:
+        crossover_point = random.randint(1, len(parent1)-1)
+        offspring = parent1[:crossover_point] + parent2[crossover_point:]
+    else:
+        offspring = parent1
+    
+    # Return the offspring genotype
+    return offspring
+
+
+def mutation_function(genotype):
+    # Define the probability of mutation
+    mutation_prob = 0.1
+    
+    # Perform random uniform mutation on the genotype with a probability of mutation_prob
+    mutated_genotype = []
+    for gene in genotype:
+        if random.random() < mutation_prob:
+            mutated_gene = gene + random.uniform(-0.1, 0.1)
+        else:
+            mutated_gene = gene
+        mutated_genotype.append(mutated_gene)
+    
+    # Return the mutated genotype
+    return mutated_genotype
+
+
+# Generate the initial population
+population = []
+for i in range(population_size):
+    genotype = [random.uniform(-1, 1) for i in range(state_space)]
+    population.append(genotype)
+
+# Run the genetic algorithm
+for i in range(num_generations):
+    # Evaluate the fitness of each individual in the population
+    fitness_scores = [fitness_function(genotype) for genotype in population]
+    
+    # Select parents for reproduction
+    parents = []
+    for j in range(population_size // 2):
+        parent1_index = selection_function(population)
+        parent2_index = selection_function(population)
+        parents.append((parent1_index, parent2_index))
+    
+    # Reproduce and mutate the genotypes
+    offspring = []
+    for parent1_index, parent2_index in parents:
+        parent1 = population[parent1_index]
+        parent2 = population[parent2_index]
+        offspring_genotype = crossover_function(parent1, parent2)
+        mutated_genotype = mutation_function(offspring_genotype)
+        offspring.append(mutated_genotype)
+    
+    # Replace the old population with the new offspring
+    population = offspring
+
+    # Print the best fitness score for each generation
+    best_fitness = max(fitness_scores)
+    print("Generation:", i+1, "Best fitness:", best_fitness)

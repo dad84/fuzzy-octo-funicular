39fc8a38b537bc9510d6a6147c04ce93515b3367
Dad <107422850+dad84@users.noreply.github.com>
2023-02-26 14:55:05 +0100

    Update and rename TestSnake.py to GeneticSnakeAI.py
    as much as i see at the moment no bugs

--- v2/GeneticSnakeAI.py
+++ v2/GeneticSnakeAI.py
@@ -1,4 +1,5 @@
 import random
+import pygame
 
 # Define the state space of the Snake game
 state_space = 4
@@ -9,11 +10,26 @@ population_size = 50
 # Define the number of generations to run the algorithm
 num_generations = 100
 
+# Define the size of the game board
+board_width = 20
+board_height = 20
+
+# Initialize the pygame library
+pygame.init()
+
+# Set the size of the game window
+window_size = (500, 500)
+screen = pygame.display.set_mode(window_size)
+
+# Set up the game window
+pygame.display.set_caption("Snake Game")
+
+# Define the colors used in the game
+background_color = (255, 255, 255)
+snake_color = (0, 0, 255)
+food_color = (255, 0, 0)
+
 def fitness_function(genotype):
-    # Define the size of the game board
-    board_width = 20
-    board_height = 20
-    
     # Define the initial position and velocity of the Snake
     snake_x = board_width // 2
     snake_y = board_height // 2
@@ -28,9 +44,13 @@ def fitness_function(genotype):
     snake_list = [(snake_x, snake_y)]
     game_over = False
     
-    # Run the game for a fixed number of steps
-    max_steps = 100
-    for step in range(max_steps):
+    # Run the game loop
+    while not game_over:
+        # Handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+        
         # Update the position of the Snake based on its velocity
         snake_x += snake_dx
         snake_y += snake_dy
@@ -53,17 +73,24 @@ def fitness_function(genotype):
         if len(snake_list) > snake_length:
             del snake_list[0]
         
-        # Update the velocity of the Snake based on its genotype
+        # Update the velocity of the Snake based on the genotype
         input_vector = [snake_x-food_x, snake_y-food_y, snake_dx, snake_dy]
         output_vector = [sum([input_vector[i]*genotype[i] for i in range(len(genotype))])]
         snake_dx += int(output_vector[0])
         snake_dy += int(output_vector[0])
         
+        # Draw the game board
+        screen.fill(background_color)
+        for segment in snake_list:
+            pygame.draw.rect(screen, snake_color, (segment[0]*20, segment[1]*20, 20, 20))
+        pygame.draw.rect(screen, food_color, (food_x*20, food_y*20, 20, 20))
+        pygame.display.update()
+        
         # Check if the game is over
         if game_over:
             break
-    
-    # Return the fitness score based on the final score of the game
+            
+        # Return the fitness score based on the final score of the game
     return score
 
 
@@ -144,3 +171,80 @@ for i in range(num_generations):
     # Print the best fitness score for each generation
     best_fitness = max(fitness_scores)
     print("Generation:", i+1, "Best fitness:", best_fitness)
+
+# Run the game with the best individual from the final generation
+best_individual = max(population, key=fitness_function)
+fitness = fitness_function(best_individual)
+
+print("Best individual:", best_individual)
+print("Fitness:", fitness)
+
+# Define the initial position and velocity of the Snake
+snake_x = board_width // 2
+snake_y = board_height // 2
+snake_dx, snake_dy = 0, -1
+
+# Define the initial position of the food
+food_x, food_y = random.randint(0, board_width-1), random.randint(0, board_height-1)
+
+# Define the initial state of the game
+score = 0
+snake_length = 1
+snake_list = [(snake_x, snake_y)]
+game_over = False
+
+# Run the game
+while not game_over:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+    
+    # Update the position of the Snake based on its velocity
+    snake_x += snake_dx
+    snake_y += snake_dy
+    
+    # Check if the Snake has collided with the walls or its own body
+    if snake_x < 0 or snake_x >= board_width or snake_y < 0 or snake_y >= board_height:
+        game_over = True
+    for segment in snake_list[:-1]:
+        if segment == (snake_x, snake_y):
+            game_over = True
+    
+    # Check if the Snake has eaten the food
+    if snake_x == food_x and snake_y == food_y:
+        score += 1
+        snake_length += 1
+        food_x, food_y = random.randint(0, board_width-1), random.randint(0, board_height-1)
+    
+    # Update the position of the Snake's body
+    snake_list.append((snake_x, snake_y))
+    if len(snake_list) > snake_length:
+        del snake_list[0]
+    
+    # Update the velocity of the Snake based on the genotype
+    input_vector = [snake_x-food_x, snake_y-food_y, snake_dx, snake_dy]
+    output_vector = [sum([input_vector[i]*best_individual[i] for i in range(len(best_individual))])]
+    snake_dx += int(output_vector[0])
+    snake_dy += int(output_vector[0])
+    
+    # Draw the game board
+    screen.fill(background_color)
+    for segment in snake_list:
+        pygame.draw.rect(screen, snake_color, (segment[0]*20, segment[1]*20, 20, 20))
+    pygame.draw.rect(screen, food_color, (food_x*20, food_y*20, 20, 20))
+    pygame.display.update()
+    
+    # Check if the game is over
+    if game_over:
+        break
+
+# Print the final score and genotype of the best individual
+print("Final score:", score)
+print("Best individual:", best_individual)
+print("Fitness:", fitness)
+
+# Quit the game
+pygame.quit()
+
+    

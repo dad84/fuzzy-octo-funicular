4eb8320822c2a01302a772198f838feaa8508b4d
Dad <107422850+dad84@users.noreply.github.com>
2023-03-01 18:53:40 +0100

    v0.2

--- btc/BTCag.py
+++ btc/BTCag.py
@@ -2,7 +2,6 @@ import numpy as np
 import requests
 import time
 
-# Gather historical data from CryptoCompare API
 historical_url = "https://min-api.cryptocompare.com/data/v2/histoday"
 params = {
     "fsym": "BTC",
@@ -12,9 +11,9 @@ params = {
 response = requests.get(historical_url, params=params)
 data = response.json()["Data"]["Data"]
 
-# Extract price and volume data from historical data
 prices = []
 volumes = []
+rsi = []
 for day in data:
     timestamp = day["time"]
     price = day["close"]
@@ -22,41 +21,49 @@ for day in data:
     prices.append(price)
     volumes.append(volume)
 
-# Define the problem
-def fitness_function(weights):
-    # Normalize the weights
+    if len(prices) > 14:
+        # Calculate RSI for the past 14 days
+        deltas = np.diff(prices[-14:])
+        seed = deltas[:1]
+        up = deltas[deltas >= 0].sum() / 14
+        down = -deltas[deltas < 0].sum() / 14
+        rs = up / down
+        rsi.append(100.0 - (100.0 / (1.0 + rs)))
+    else:
+        rsi.append(np.nan)
+
+print("Historical data collected")
+
+
+def fitness_function(weights, rsi):
     weights = np.array(weights) / np.sum(weights)
-    # Use the weights to predict the next day's price
     prediction = np.dot(prices[-7:], weights)
     actual = prices[-1]
-    # Calculate the fitness as the inverse of the absolute error
-    fitness = 1 / abs(prediction - actual)
+    rsi_value = rsi[-1]
+    fitness = 1 / abs(prediction - actual) * rsi_value
     return fitness
 
-# Create the GA program
-population_size = 50
+population_size = 500
 num_weights = 7
-mutation_rate = 0.1
-num_sets = 5 # Number of sets of weights to use for ensemble
+mutation_rate = 0.01
+num_sets = 5
 
-population = np.random.rand(population_size, num_weights)
+population = np.random.rand(population_size, num_weights + 1)
 fitness_scores = np.zeros(population_size)
 
 best_fitness = 0
 best_weights = None
 
+counter = 0
+
 while True:
-    # Evaluate fitness for each individual in the population
     for i in range(population_size):
-        fitness_scores[i] = fitness_function(population[i])
-    # Find the top n individuals in the population
+        fitness_scores[i] = fitness_function(population[i, :-1], rsi)
     top_indices = np.argsort(fitness_scores)[-num_sets:]
-    top_weights = population[top_indices]
-    # Use ensemble methods to predict future prices
+    top_weights = population[top_indices, :-1]
     predictions = np.zeros(num_sets)
     for i in range(num_sets):
         predictions[i] = np.dot(prices[-7:], top_weights[i])
-    # Gather current market data from CryptoCompare API
     current_url = "https://min-api.cryptocompare.com/data/price"
     params = {
         "fsym": "BTC",
@@ -67,37 +74,51 @@ while True:
         current_data = response.json()
         if "USD" in current_data:
             current_price = current_data["USD"]
+            current_volume = volumes[-1]
+            future_price = np.mean(predictions) * (current_price / prices[-1])
+            if np.max(fitness_scores) > best_fitness:
+                best_fitness = np.max(fitness_scores)
+                best_weights = population[np.argmax(fitness_scores), :-1]
+                print("New best weights found! Fitness score:", best_fitness)
         else:
-            print("Error: no USD data found in API response.")
-            continue
+            raise ValueError("Error: no USD data found in API response.")
     else:
         print("Error: API request failed.")
         continue
-    current_volume = volumes[-1]  # We can use the last known volume
+    counter += 1
+    if counter > 100:
+        break
+    time.sleep(30)
+    current_volume = volumes[-1]
     future_price = np.mean(predictions) * (current_price / prices[-1])
-    # Update best fitness and weights
     if np.max(fitness_scores) > best_fitness:
         best_fitness = np.max(fitness_scores)
-        best_weights = population[np.argmax(fitness_scores)]
-    # Create a new population through selection and mutation
-    new_population = np.zeros((population_size, num_weights))
+        best_weights = population[np.argmax(fitness_scores), :-1]
+    new_population = np.zeros((population_size, num_weights + 1))
     for i in range(population_size):
-        # Tournament selection
         parent_indices = np.random.choice(population_size, size=2, replace=False)
         if fitness_scores[parent_indices[0]] > fitness_scores[parent_indices[1]]:
             parent = population[parent_indices[0]]
         else:
             parent = population[parent_indices[1]]
-        # Mutation
         mutation_mask = np.random.rand(num_weights) < mutation_rate
         mutation_amounts = np.random.normal(size=num_weights)
-        child = parent + mutation_mask * mutation_amounts
-        new_population[i] = child
+        child = parent[:-1] + mutation_mask * mutation_amounts
+        new_population[i, :-1] = child
+        new_population[i, -1] = rsi[-1]
     population = new_population
     
-    # Wait for 30 seconds before updating current market data
-    time.sleep(30)
-    
+    print("Current market data collected")
     print("Current price:", current_price)
     print("Current volume:", current_volume)
     print("Predicted future price:", future_price)
+    
+if np.max(fitness_scores) > best_fitness:
+    best_fitness = np.max(fitness_scores)
+    best_weights = population[np.argmax(fitness_scores), :-1]
+    print("New best weights found! Fitness score:", best_fitness)
+save_result = np.savetxt("best_weights.txt", best_weights)
+if save_result is None:
+    print("Best weights saved to file.")
+else:
+    print("Error saving best weights to file:", save_result)
